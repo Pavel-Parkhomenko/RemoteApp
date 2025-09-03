1ba67fdea262e1ab9d050fa5e23ad7d0752fed54
PavelParkhomenko <pavel.parkhomenko11@gmail.com>
2025-09-03 18:07:26 +0300

    add .bat

---
+++ start.bat
@@ -0,0 +1,9 @@
+@echo off
+echo Запуск AutoHotkey скрипта...
+start "" "D:\programs\RemoteApp\file.ahk"
+
+echo Запуск Node.js сервера...
+cd /d D:\programs\RemoteApp\server.js
+start "" cmd /k "node server.js"
+
+echo Всё запущено!
