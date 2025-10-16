1547d9bc7d33bbe599838fdbf1acad0fa0e3b867
PavelParkhomenko <pavel.parkhomenko11@gmail.com>
2025-10-16 19:58:18 +0300

    add new vid-s

--- start.bat
+++ start.bat
@@ -1,10 +1,12 @@
 @echo off
+chcp 65001
 echo Запуск AutoHotkey скрипта...
 start "" "D:\programs\RemoteApp\operaGXscript.ahk"
 
 echo Запуск Node.js сервера...
-echo Приложение доступно в вашем браузере по адресу
-cd /d D:\programs\RemoteApp\server.js
-start "" cmd /k "node server.js"
+cd /d D:\programs\RemoteApp
+echo Приложение доступно в вашем браузере по адресу:
+node server.js
 
 echo Сервер остановлен
+pause
